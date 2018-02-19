448e547fa4cc29ef1770c992ee731d7f8e2008ac
Brian McKenna <brian@brianmckenna.org>
2018-02-19 00:34:28 -0800

    Switch to CMake build

--- default.nix
+++ default.nix
@@ -2,18 +2,6 @@ with import <nixpkgs> { };
 
 stdenv.mkDerivation {
   name = "nix-script-store-plugin";
-  buildInputs = [ nixUnstable pkgconfig ];
+  buildInputs = [ nixUnstable pkgconfig cmake ];
   src = ./.;
-  buildPhase = ''
-    $CXX \
-      -std=c++14 -g -Wall -include config.h \
-      $(pkg-config --cflags --libs nix-store) \
-      -shared \
-      -o script-store-plugin.so \
-      script.cc script-store.cc
-  '';
-  installPhase = ''
-    mkdir -p $out/lib
-    mv script-store-plugin.so $out/lib/
-  '';
 }
